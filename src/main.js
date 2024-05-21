b55aaefa371d119ede00a0f5bdf1d510799f9a15
maru <maru@gmail.com>
2024-05-21 19:09:47 +0600

    update modal with alert

--- src/main.js
+++ src/main.js
@@ -1,6 +1,10 @@
 const modal_btn =document.getElementById("modal-btn");
 const modal =document.getElementById("modal");
-const close_modal =document.getElementById("close-modal");
+const keep =document.getElementById("keep");
+const discard =document.getElementById("discard");
+const close_modal =document.querySelector(".modal .close-modal");
+const alertCloseModal =document.querySelector(".alert .close");
+const alert =document.querySelector(".alert");
 
 
 //open modal
@@ -11,6 +15,31 @@ modal_btn.onclick = (e)=>{
 
 //close modal
 close_modal.onclick = (e)=>{
+    modal.style.opacity=1;
+    modal.style.pointerEvents="none";
+    alert.style.display="flex";
+    
+}
+alertCloseModal.onclick = (e)=>{
+    modal.style.opacity=1;
+    modal.style.pointerEvents="auto";
+    alert.style.display="none";
+    
+}
+keep.onclick = (e)=>{
+    modal.style.opacity=1;
+    modal.style.pointerEvents="auto";
+    alert.style.display="none";
+    
+}
+discard.onclick = (e)=>{
+  
     modal.style.opacity=0;
     modal.style.pointerEvents="none";
+    alert.style.display="none";
+    
+   
+    
 }
+
+
